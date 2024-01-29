c832abca9d144705e74579ea29db6afe57baebe9
Matthew Theseira <matthew.theseira@fdmgroup.com>
2024-01-30 10:11:13 +1100

    added book page and javascript code

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+//open menu of navbar
 function openMenu() {
   document.body.classList += " menu--open";
 }
 
+//close menu of navbar
 function closeMenu() {
   document.body.classList.remove("menu--open");
 }
